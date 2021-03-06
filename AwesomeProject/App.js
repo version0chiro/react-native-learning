b5ebcb4dc2c65b8d1a262676f3ccecff7c835c08
Chiro <sachinsinghroxx@gmail.com>
2021-03-06 15:13:34 +0530

    adding online

---
+++ AwesomeProject/App.js
@@ -0,0 +1,56 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  Button,
+  Alert,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+const App: () => React$Node = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.red}>just red</Text>
+      <Text style={styles.bigBlue}>just bigBlue</Text>
+      <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
+      <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+container: {
+  marginTop: 50,
+},
+bigBlue: {
+  color: 'blue',
+  fontWeight: 'bold',
+  fontSize: 30,
+},
+red: {
+  color: 'red',
+},
+});
+
+export default App;
